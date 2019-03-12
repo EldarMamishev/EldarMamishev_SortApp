a3df1fbeafe81419510ff03258136f7bf910c385
eldar.mamishev <e.mamishevjob@gmail.com>
2019-03-12 13:50:39 +0200

    Added interfaces

---
+++ SortProgram/Business/Sort/Interface/ISortHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Enum;
+
+namespace Business.Sort.Interface
+{
+    interface ISortHandler
+    {
+        ISortResult Handle (string sequence, SortAlgorithmEnum sortAlgorithm, SortTypeEnum sortType);
+    }
+}
---
+++ SortProgram/Business/Sort/Interface/ISortResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Sort.Interface
+{
+    interface ISortResult
+    {
+        IEnumerable<decimal> SortedNumbers { get; }
+        int SwapOperationsCount { get; }
+        int CompareOperationsCount { get; }
+    }
+}
