4f5fc05e8d48720085c0d1174eea0e4b5a2760c3
eldar.mamishev <e.mamishevjob@gmail.com>
2019-03-12 13:51:13 +0200

    Added ViewModel files

---
+++ SortProgram/ViewModel/Views/InputControlsViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using ViewModel.Base;
+
+namespace ViewModel.Views
+{
+    class InputControlsViewModel : ViewModelBase
+    {
+        public string InputSequence
+        {
+            get
+            {
+                return this.InputSequence;
+            }
+
+            set
+            {
+                this.InputSequence = value;
+                OnPropertyChanged("InputSequence");
+            }
+        }
+
+    }
+}
---
+++ SortProgram/ViewModel/Views/SettingControlsViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Business.Enum;
+using ViewModel.Base;
+
+namespace ViewModel.Views
+{
+    class SettingControlsViewModel : ViewModelBase
+    {
+        public SortTypeEnum SelectedSortType
+        {
+            get
+            {
+                return this.SelectedSortType;
+            }
+            set
+            {
+                this.SelectedSortType = value;
+                OnPropertyChanged("SelectedSortType");
+            }
+        }  
+
+        public SortAlgorithmEnum SelectedSortAlgorithm
+        {
+            get
+            {
+                return this.SelectedSortAlgorithm;
+            }
+            set
+            {
+                this.SelectedSortAlgorithm = value;
+                OnPropertyChanged("SelectedSortAlgorithm");
+            }
+        }
+    }
+}
