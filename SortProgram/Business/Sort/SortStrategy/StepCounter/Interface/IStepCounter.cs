345e9241dd4b714c0e5e70ee075a31d7ac512b9e
eldar.mamishev <e.mamishevjob@gmail.com>
2019-03-12 15:30:32 +0200

    Added all interfaces in Business

---
+++ SortProgram/Business/Sort/SortStrategy/StepCounter/Interface/IStepCounter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Sort.SortStrategy.StepCounter.Interface
+{
+    public interface IStepCounter
+    {
+        int CompareOperationsCount { get; }
+        void CountSwapOperation ();
+        void CountCompareOperation ();
+        int SwapOperationsCount { get; }
+    }
+}
---
+++ SortProgram/Business/Sort/SortType/Interface/ISortType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Sort.SortType.Interface
+{
+    public interface ISortType
+    {
+        IEnumerable<decimal> Update(IEnumerable<decimal> sortedFrequence);
+    }
+}
