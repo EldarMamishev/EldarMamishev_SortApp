3450de1aae55ac7b035438bdd5a34e2240b50577
eldar.mamishev <e.mamishevjob@gmail.com>
2019-03-13 16:08:37 +0200

    Added SortStrategyFactory

---
+++ SortProgram/Business/Sort/SortStrategy/Factory/SortStrategyFactory.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Sort.Enum;
+using Business.Sort.SortStrategy.Factory.Interface;
+using Business.Sort.SortStrategy.Interface;
+using Business.Sort.SortType.Factory;
+using Business.Sort.SortType.Factory.Interface;
+using Business.Sort.SortType.Interface;
+
+namespace Business.Sort.SortStrategy.Factory
+{
+    class SortStrategyFactory : ISortStrategyFactory
+    {
+        private ISortTypeFactory sortTypeFactory;
+        public ISortStrategy CreateSort(SortAlgorithmEnum sortAlgorithm, SortTypeEnum sortType)
+        {
+            sortTypeFactory = new SortTypeFactory();
+            ISortType neededSortType = sortTypeFactory.CreateSortType(sortType);
+            switch (sortAlgorithm)
+            {
+                case SortAlgorithmEnum.InsertionSort:
+                {
+                    return new InsertionSort(neededSortType);
+                }
+                case SortAlgorithmEnum.MergeSort:
+                {
+                    return new MergeSort(neededSortType);
+                }
+                case SortAlgorithmEnum.QuickSort:
+                {
+                    return new QuickSort(neededSortType);
+                }
+                case SortAlgorithmEnum.SelectionSort:
+                {
+                    return new SelectionSort(neededSortType);
+                }
+                default:
+                    return null;
+            }
+        }
+    }
+}
