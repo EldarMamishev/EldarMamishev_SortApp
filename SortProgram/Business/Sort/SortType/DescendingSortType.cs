8a83a476be36c0ec0354e8bae092678bf827cc0c
eldar.mamishev <e.mamishevjob@gmail.com>
2019-03-13 15:41:05 +0200

    Added Asc and Desc SortType classes

---
+++ SortProgram/Business/Sort/SortType/DescendingSortType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Sort.SortType.Interface;
+
+namespace Business.Sort.SortType
+{
+    class DescendingSortType : ISortType
+    {
+        public IEnumerable<decimal> Update(IEnumerable<decimal> sortedSequence)
+        {
+            return sortedSequence.Reverse();
+        }
+    }
+}
