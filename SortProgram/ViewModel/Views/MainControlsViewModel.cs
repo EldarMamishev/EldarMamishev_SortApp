d8a7b7054ef9e1a906fe13d0732996091e188a3e
eldar.mamishev <e.mamishevjob@gmail.com>
2019-03-14 12:34:16 +0200

    Added features to Main and Output ViewModels

--- SortProgram/ViewModel/Views/MainControlsViewModel.cs
+++ SortProgram/ViewModel/Views/MainControlsViewModel.cs
@@ -4,11 +4,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ViewModel.Base;
+using ViewModel.Command;
 
 namespace ViewModel.Views
 {
     public sealed class MainControlsViewModel : ViewModelBase
     {
+        private InputControlsViewModel inputControlsVM;
+        private OutputControlsViewModel outputControlsVM;
+        private SettingControlsViewModel settingControlsVM;
+        private RelayCommand sortCommand;
 
+        public InputControlsViewModel InputControlsVM
+        {
+            get
+            {
+                return inputControlsVM ?? 
+                    (inputControlsVM = new InputControlsViewModel());
+            }
+        }
+
+        public OutputControlsViewModel OutputControlsVM
+        {
+            get
+            {
+                return outputControlsVM ??
+                    (outputControlsVM = new OutputControlsViewModel());
+            }
+        }
+
+        public SettingControlsViewModel SettingControlsVM
+        {
+            get
+            {
+                return settingControlsVM ??
+                    (settingControlsVM = new SettingControlsViewModel());
+            }
+        }
+
+        private RelayCommand SortCommand
+        {
+            get
+            {
+                return sortCommand ?? 
+                    (sortCommand = new RelayCommand(
+                        obj => OutputControlsVM.Sort(InputControlsVM.InputSequence, SettingControlsVM.SelectedSortAlgorithm, SettingControlsVM.SelectedSortType), 
+                        obj => InputControlsVM.InputSequence.Length > 0));
+            }
+        }
     }
 }
--- SortProgram/ViewModel/Views/OutputControlsViewModel.cs
+++ SortProgram/ViewModel/Views/OutputControlsViewModel.cs
@@ -3,12 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Business.Sort;
+using Business.Sort.Enum;
+using Business.Sort.Interface;
+using Business.Sort.Parse;
+using Business.Sort.Parse.Interface;
 using ViewModel.Base;
 
 namespace ViewModel.Views
 {
     public sealed class OutputControlsViewModel : ViewModelBase
     {
+        private ISortResult sortResult;
+        private ISortHandler sortHandler;
+        private IStringToCollectionParser<decimal> stringToCollectionParser;
+        
+        public int CompareOperationsCount
+        {
+            get
+            {
+                return sortResult.CompareOperationsCount;
+            }
+        }
 
+        public OutputControlsViewModel()
+        {
+            sortHandler = new SortHandler();
+            stringToCollectionParser = new StringToDecimalCollectionParser();
+        }
+
+        public string OutputSequence
+        {
+            get
+            {
+                return stringToCollectionParser.ParseCollectionToString(sortResult.SortedNumbers);
+            }
+        }
+
+        public int SwapOperationsCount
+        {
+            get
+            {
+                return sortResult.SwapOperationsCount;
+            }
+        }               
+
+        public void Sort(string sequence, SortAlgorithmEnum sortAlgorithm, SortTypeEnum sortType)
+        {
+            sortResult = sortHandler.Handle(sequence, sortAlgorithm, sortType);
+        }
     }
 }
