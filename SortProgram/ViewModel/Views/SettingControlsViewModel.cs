bf61b24f4fe1330991563ee6ca02e5d843c638e0
eldar.mamishev <e.mamishevjob@gmail.com>
2019-03-14 18:10:49 +0200

    Made listView work

--- SortProgram/ViewModel/Views/SettingControlsViewModel.cs
+++ SortProgram/ViewModel/Views/SettingControlsViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Business.Sort.Enum;
 using ViewModel.Base;
 
@@ -6,6 +7,11 @@ namespace ViewModel.Views
 {
     public sealed class SettingControlsViewModel : ViewModelBase
     {
+        //private KeyValuePair<string, SortAlgorithmEnum> selectedSortAlgorithm;
+        //private KeyValuePair<string, SortTypeEnum> selectedSortType;
+        private Dictionary<string, SortAlgorithmEnum> sortAlgorithms;
+        private Dictionary<string, SortTypeEnum> sortTypes;
+
         private SortAlgorithmEnum selectedSortAlgorithm;
         private SortTypeEnum selectedSortType;
 
@@ -33,6 +39,52 @@ namespace ViewModel.Views
                 this.selectedSortType = value;
                 this.OnPropertyChanged(nameof(this.SelectedSortType));
             }
-        }          
+        }
+
+        //public KeyValuePair<string, SortAlgorithmEnum> SelectedSortAlgorithm
+        //{
+        //    get
+        //    {
+        //        return this.selectedSortAlgorithm;
+        //    }
+        //    set
+        //    {
+        //        this.selectedSortAlgorithm = value;
+        //        this.OnPropertyChanged(nameof(this.SelectedSortAlgorithm));
+        //    }
+        //}
+
+        //public KeyValuePair<string, SortTypeEnum> SelectedSortType
+        //{
+        //    get
+        //    {
+        //        return this.selectedSortType;
+        //    }
+        //    set
+        //    {
+        //        this.selectedSortType = value;
+        //        this.OnPropertyChanged(nameof(this.SelectedSortType));
+        //    }
+        //}
+
+        public SettingControlsViewModel()
+        {
+            sortAlgorithms = new Dictionary<string, SortAlgorithmEnum>();
+            sortTypes = new Dictionary<string, SortTypeEnum>();
+            foreach(SortAlgorithmEnum sort in Enum.GetValues(typeof(SortAlgorithmEnum)))
+            {
+                sortAlgorithms.Add(sort.ToString(), sort);
+            }
+
+            foreach (SortTypeEnum sort in Enum.GetValues(typeof(SortTypeEnum)))
+            {
+                sortTypes.Add(sort.ToString(), sort);
+            }
+        }
+
+        public Dictionary<string, SortAlgorithmEnum> SortAlgorithms => this.sortAlgorithms;
+
+        public Dictionary<string, SortTypeEnum> SortTypes => this.sortTypes;
+
     }
 }
