5e7381f9f69e4a86bfb094a6a6e7f3265f57c65f
eldar.mamishev <e.mamishevjob@gmail.com>
2019-03-15 15:57:16 +0200

    Changed test projects from NUnit to MSUnit, added tests for all Invalid situations in StringToDecimalValidator class

--- SortProgram/Tests/Business.UnitTest/Sort/Parse/StringToDecimalValidatortest.cs
+++ SortProgram/Tests/Business.UnitTest/Sort/Parse/StringToDecimalValidatortest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Business.Sort.Parse.Exception;
 using Business.Sort.Parse;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -15,6 +16,70 @@ namespace Business.UnitTest.Sort.Parse
             var validator = new StringToDecimalValidator();
 
             Assert.ThrowsException<ArgumentNullException>(() => validator.Validate(null));
+        }
+
+        [TestMethod]
+        public void StringToDecimalValidator_Validate_SetEmptyString_ThrowsValidationException()
+        {
+            var validator = new StringToDecimalValidator();
+
+            Assert.ThrowsException<ValidationException>(() => validator.Validate(string.Empty));
+        }
+
+        [TestMethod]
+        public void StringToDecimalValidator_Validate_SetStringWithLetters_ThrowsValidationException()
+        {
+            var validator = new StringToDecimalValidator();
+
+            Assert.ThrowsException<ValidationException>(() => validator.Validate("a"));
+        }
+
+        [TestMethod]
+        public void StringToDecimalValidator_Validate_SetUnsupportedCharacters_ThrowsValidationException()
+        {
+            var validator = new StringToDecimalValidator();
+
+            Assert.ThrowsException<ValidationException>(() => validator.Validate(","));
+        }
+
+        [TestMethod]
+        public void StringToDecimalValidator_Validate_SetOnlyADot_ThrowsValidationException()
+        {
+            var validator = new StringToDecimalValidator();
+
+            Assert.ThrowsException<ValidationException>(() => validator.Validate("."));
+        }
+
+        [TestMethod]
+        public void StringToDecimalValidator_Validate_SetNumberStartingWithDot_ThrowsValidationException()
+        {
+            var validator = new StringToDecimalValidator();
+
+            Assert.ThrowsException<ValidationException>(() => validator.Validate(".1"));
+        }
+
+        [TestMethod]
+        public void StringToDecimalValidator_Validate_SetNumberEndingWithDot_ThrowsValidationException()
+        {
+            var validator = new StringToDecimalValidator();
+
+            Assert.ThrowsException<ValidationException>(() => validator.Validate("1."));
+        }
+
+        [TestMethod]
+        public void StringToDecimalValidator_Validate_SetWhiteSpaces_ThrowsValidationException()
+        {
+            var validator = new StringToDecimalValidator();
+
+            Assert.ThrowsException<ValidationException>(() => validator.Validate(" "));
+        }
+
+        [TestMethod]
+        public void StringToDecimalValidator_Validate_SetNumberWithTwoDots_ThrowsValidationException()
+        {
+            var validator = new StringToDecimalValidator();
+
+            Assert.ThrowsException<ValidationException>(() => validator.Validate("1.1."));
         }
     }
 }
