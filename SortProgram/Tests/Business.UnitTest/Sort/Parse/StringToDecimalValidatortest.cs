7ea5db6f8d2674b35ba85b7b5f791c0598c52b06
eldar.mamishev <e.mamishevjob@gmail.com>
2019-03-15 15:27:55 +0200

    d

---
+++ SortProgram/Tests/Business.UnitTest/Sort/Parse/StringToDecimalValidatortest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Business.Sort.Parse;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Business.UnitTest.Sort.Parse
+{
+    [TestClass]
+    public class StringToDecimalValidatorTest
+    {
+        [TestMethod]
+        public void StringToDecimalValidator_Validate_SetNull_ThrowsArgumentNullException()
+        {
+            var validator = new StringToDecimalValidator();
+
+            Assert.ThrowsException<ArgumentNullException>(() => validator.Validate(null));
+        }
+    }
+}
