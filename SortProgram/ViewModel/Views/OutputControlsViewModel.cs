9a6a0c2a5b51332f6a9ad0660be51604e08c91e4
eldar.mamishev <e.mamishevjob@gmail.com>
2019-03-15 12:53:32 +0200

    Added labels and exception handling

--- SortProgram/ViewModel/Views/OutputControlsViewModel.cs
+++ SortProgram/ViewModel/Views/OutputControlsViewModel.cs
@@ -7,6 +7,7 @@ using Business.Sort;
 using Business.Sort.Enum;
 using Business.Sort.Interface;
 using Business.Sort.Parse;
+using Business.Sort.Parse.Exception;
 using Business.Sort.Parse.Interface;
 using ViewModel.Base;
 
@@ -17,11 +18,19 @@ namespace ViewModel.Views
         private ISortResult sortResult;
         private ISortHandler sortHandler;
         private IStringToCollectionParser<decimal> stringToCollectionParser;
-        private string outputSequence;
-        private int swapOperationsCount;
-        private int compareOperationsCount;
+        private string errorMessage;
+        
+        public int? CompareOperationsCount
+        {
+            get
+            {
+                if (sortResult == null)
+                    return null;
+                return this.sortResult.CompareOperationsCount;
+            }
+        }
 
-        public int CompareOperationsCount => this.compareOperationsCount;
+        public string ErrorMessage => errorMessage ?? string.Empty;
 
         public OutputControlsViewModel()
         {
@@ -29,17 +38,47 @@ namespace ViewModel.Views
             this.stringToCollectionParser = new StringToDecimalCollectionParser();
         }
 
-        public string OutputSequence => this.outputSequence;
-
-        public int SwapOperationsCount => this.swapOperationsCount;
-
+        public string OutputSequence
+        {
+            get
+            {
+                if(sortResult == null)
+                    return string.Empty;
+                return this.stringToCollectionParser.ParseCollectionToString(this.sortResult.SortedNumbers);
+            }
+        }
+        
         public void Sort(string sequence, SortAlgorithmEnum sortAlgorithm, SortTypeEnum sortType)
         {
-            this.sortResult = this.sortHandler.Handle(sequence, sortAlgorithm, sortType);
-            this.outputSequence = this.stringToCollectionParser.ParseCollectionToString(this.sortResult.SortedNumbers);
-            this.compareOperationsCount = this.sortResult.CompareOperationsCount;
-            this.swapOperationsCount = this.sortResult.SwapOperationsCount;
+            try
+            {   
+                this.errorMessage = string.Empty;
+                this.sortResult = this.sortHandler.Handle(sequence, sortAlgorithm, sortType); 
+            }
+            catch(NullReferenceException)
+            {
+                errorMessage = "Field was empty";
+            }
+            catch(ValidationException)
+            {
+                errorMessage = "Not validating sequence";
+            }
+            catch(Exception)
+            {
+                errorMessage = "Error!";
+            }
+
             this.OnPropertyChanged(string.Empty);
+        }
+
+        public int? SwapOperationsCount
+        {
+            get
+            {
+                if (sortResult == null)
+                    return null;
+                return this.sortResult.SwapOperationsCount;
+            }
         }
     }
 }
