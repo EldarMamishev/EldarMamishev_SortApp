764006afcba6eef3d3d0b914f09e3cf690f1fa86
eldar.mamishev <e.mamishevjob@gmail.com>
2019-03-18 18:59:18 +0200

    Added tests for all classes in SortStrategy namespace

---
+++ SortProgram/Tests/Business.UnitTest/Sort/SortStrategy/Base/SortStrategyBaseTest.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Business.Sort.SortType.Interface;
+using Business.Sort.SortStrategy;
+using Business.Sort;
+using Business.Sort.Interface;
+using Business.Sort.SortStrategy.Base;
+using Business.Sort.SortStrategy.StepCounter.Interface;
+
+namespace Business.UnitTest.Sort.SortStrategy.Base
+{
+    [TestClass]
+    public abstract class SortStrategyBaseTest
+    {
+        protected abstract SortStrategyBase GetSortStrategy(ISortType sortType, IStepCounter stepCounter);
+
+        [TestMethod]        
+        public void SortStrategy_Sort_SequenceIsNull_ThrowArgumentNullException()
+        {
+            ISortType sortType = new Business.Sort.SortType.Interface.Fakes.StubISortType()
+            {
+                UpdateIEnumerableOfDecimal = (notUpdatedSequence) => { return notUpdatedSequence; }
+            };
+
+            IStepCounter stepCounter = new Business.Sort.SortStrategy.StepCounter.Interface.Fakes.StubIStepCounter()
+            {
+                CountSwapOperation = () => { return; },
+                CountCompareOperation = () => { return; }
+            };
+
+            var sortStrategy = GetSortStrategy(sortType, stepCounter);
+
+            Assert.ThrowsException<ArgumentNullException>(() => sortStrategy.Sort(null));
+        }
+
+        [TestMethod]
+        public void SortStrategy_Sort_SequenceIsEmpty_ReturnEmptySequence()
+        {
+            ISortType sortType = new Business.Sort.SortType.Interface.Fakes.StubISortType()
+            {
+                UpdateIEnumerableOfDecimal = (notUpdatedSequence) => { return notUpdatedSequence; }
+            };
+
+            IStepCounter stepCounter = new Business.Sort.SortStrategy.StepCounter.Interface.Fakes.StubIStepCounter()
+            {
+                CountSwapOperation = () => { return; },
+                CountCompareOperation = () => { return; }
+            };
+
+            var sortStrategy = GetSortStrategy(sortType, stepCounter);
+            var expectedSequence = new decimal[1];
+
+            ISortResult sortResult = sortStrategy.Sort((new decimal[1]));
+
+            Assert.IsTrue(expectedSequence.SequenceEqual(sortResult.SortedNumbers));
+        }
+
+        [TestMethod]
+        public void SortStrategy_Sort_SequenceWithUnsortedNumbers_ReturnSortedSequence()
+        {
+            ISortType sortType = new Business.Sort.SortType.Interface.Fakes.StubISortType()
+            {
+                UpdateIEnumerableOfDecimal = (notUpdatedSequence) => { return notUpdatedSequence; }
+            };
+
+            IStepCounter stepCounter = new Business.Sort.SortStrategy.StepCounter.Interface.Fakes.StubIStepCounter()
+            {
+                CountSwapOperation = () => { return; },
+                CountCompareOperation = () => { return; }
+            };
+
+            var sortStrategy = GetSortStrategy(sortType, stepCounter);
+            var expectedSequence = new decimal[] {1, 2, 3, 4};
+            var primarySequence = new decimal[] {4, 2, 1, 3};
+
+            ISortResult sortResult = sortStrategy.Sort(primarySequence);
+
+            Assert.IsTrue(expectedSequence.SequenceEqual(sortResult.SortedNumbers));
+        }
+    }    
+}
---
+++ SortProgram/Tests/Business.UnitTest/Sort/SortStrategy/Factory/SortStrategyFactoryTest.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Sort.SortStrategy.Factory;
+using Business.Sort.SortType.Factory.Interface;
+using Business.Sort.SortType.Interface;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Business.Sort.Enum;
+using Business.Sort.SortStrategy.StepCounter.Interface;
+using Business.Sort.SortStrategy.Interface;
+using Business.Sort.SortStrategy;
+
+namespace Business.UnitTest.Sort.SortStrategy.Factory
+{
+    [TestClass]
+    public class SortStrategyFactoryTest
+    {
+        private ISortTypeFactory CreateSortTypeFactory()
+        {
+            ISortTypeFactory sortTypeFactory = new Business.Sort.SortType.Factory.Interface.Fakes.StubISortTypeFactory()
+            {
+                CreateSortTypeSortTypeEnum = (sortTypeEnum) => {
+                    return new Business.Sort.SortType.Interface.Fakes.StubISortType()
+                    {
+                        UpdateIEnumerableOfDecimal = (notUpdatedSequence) => { return notUpdatedSequence; }
+
+                    };
+                }
+            };
+
+            return sortTypeFactory;
+        }
+
+        [TestMethod]
+        public void SortStrategyFactory_CreateSort_InsertionSortValue_CreateInsertionSortInstance()
+        {
+            ISortTypeFactory sortTypeFactory = CreateSortTypeFactory();
+
+            var sortStrategyFactory = new SortStrategyFactory(sortTypeFactory);
+
+            IStepCounter stepCounter = new Business.Sort.SortStrategy.StepCounter.Interface.Fakes.StubIStepCounter()
+            {
+                CountSwapOperation = () => { return; },
+                CountCompareOperation = () => { return; }
+            };
+
+            ISortStrategy sortStrategy = sortStrategyFactory.CreateSort(SortAlgorithmEnum.InsertionSort, SortTypeEnum.Ascending, stepCounter);
+
+            Assert.IsInstanceOfType(sortStrategy, typeof(InsertionSort));
+        }
+
+        [TestMethod]
+        public void SortStrategyFactory_CreateSort_SelectionSortValue_CreateSelectionSortInstance()
+        {
+            ISortTypeFactory sortTypeFactory = CreateSortTypeFactory();
+
+            var sortStrategyFactory = new SortStrategyFactory(sortTypeFactory);
+
+            IStepCounter stepCounter = new Business.Sort.SortStrategy.StepCounter.Interface.Fakes.StubIStepCounter()
+            {
+                CountSwapOperation = () => { return; },
+                CountCompareOperation = () => { return; }
+            };
+
+            ISortStrategy sortStrategy = sortStrategyFactory.CreateSort(SortAlgorithmEnum.SelectionSort, SortTypeEnum.Ascending, stepCounter);
+
+            Assert.IsInstanceOfType(sortStrategy, typeof(SelectionSort));
+        }
+
+        [TestMethod]
+        public void SortStrategyFactory_CreateSort_QuickSortValue_CreateQuickSortInstance()
+        {
+            ISortTypeFactory sortTypeFactory = CreateSortTypeFactory();
+
+            var sortStrategyFactory = new SortStrategyFactory(sortTypeFactory);
+
+            IStepCounter stepCounter = new Business.Sort.SortStrategy.StepCounter.Interface.Fakes.StubIStepCounter()
+            {
+                CountSwapOperation = () => { return; },
+                CountCompareOperation = () => { return; }
+            };
+
+            ISortStrategy sortStrategy = sortStrategyFactory.CreateSort(SortAlgorithmEnum.QuickSort, SortTypeEnum.Ascending, stepCounter);
+
+            Assert.IsInstanceOfType(sortStrategy, typeof(QuickSort));
+        }
+
+        [TestMethod]
+        public void SortStrategyFactory_CreateSort_MergeSortValue_CreateMergeSortInstance()
+        {
+            ISortTypeFactory sortTypeFactory = CreateSortTypeFactory();
+
+            var sortStrategyFactory = new SortStrategyFactory(sortTypeFactory);
+
+            IStepCounter stepCounter = new Business.Sort.SortStrategy.StepCounter.Interface.Fakes.StubIStepCounter()
+            {
+                CountSwapOperation = () => { return; },
+                CountCompareOperation = () => { return; }
+            };
+
+            ISortStrategy sortStrategy = sortStrategyFactory.CreateSort(SortAlgorithmEnum.MergeSort, SortTypeEnum.Ascending, stepCounter);
+
+            Assert.IsInstanceOfType(sortStrategy, typeof(MergeSort));
+        }
+    }
+}
